6f396c7c96f812e0ab7c4210046cf1df9d3e27f5
sungmin <95615086+Clariatinet@users.noreply.github.com>
2023-05-20 23:51:32 +0900

    5/20

--- react-complete-guide/src/components/Expenses/ExpenseItem.jsx
+++ react-complete-guide/src/components/Expenses/ExpenseItem.jsx
@@ -19,7 +19,6 @@ const ExpenseItem = (props) => {
         <div className='expense-item__price'>${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
